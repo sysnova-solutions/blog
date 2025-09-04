a7ed77ec7270bac184a7da9caca905599e8b1535
Suraj Shetty <surajshetty3416@gmail.com>
2025-09-04 23:03:57 +0530

    feat: Add builder as dependency

--- blog/hooks.py
+++ blog/hooks.py
@@ -9,6 +9,7 @@ app_license = "mit"
 # ------------------
 
 # required_apps = []
+required_apps = ["builder"]
 
 # Each item in the list will be shown as an app in the apps page
 add_to_apps_screen = [
