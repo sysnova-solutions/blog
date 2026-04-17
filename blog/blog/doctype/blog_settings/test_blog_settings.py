2dc714577cc78bd9d410b43861c4dbfdda634d8b
AarDG10 <aarol.dsouza@gmail.com>
2026-04-17 13:24:21 +0530

    test: fix test failures

--- blog/blog/doctype/blog_settings/test_blog_settings.py
+++ blog/blog/doctype/blog_settings/test_blog_settings.py
@@ -12,14 +12,14 @@ from blog.blog.doctype.blog_post.test_blog_post import make_test_blog
 class TestBlogSettings(IntegrationTestCase):
 	@IntegrationTestCase.change_settings("Blog Settings", {"allow_guest_to_comment": 0})
 	def test_guest_cannot_comment(self):
-		some_system_user = frappe.db.get_value("User", {"name": ("not in", frappe.STANDARD_USERS)})
 		test_blog = make_test_blog()
+
 		with set_user("Guest"):
 			self.assertRaises(
 				frappe.ValidationError,
 				add_comment,
 				comment="Good comment with 10 chars",
-				comment_email=some_system_user,
+				comment_email="test-guest@example.com",
 				comment_by="Good Tester",
 				reference_doctype="Blog Post",
 				reference_name=test_blog.name,
